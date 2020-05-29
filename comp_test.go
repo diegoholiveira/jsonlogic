57143ca04bc08d865ec590ddac26f9cf4ce2d146
fifthaxe <25035563+fifthaxe@users.noreply.github.com>
2020-05-29 16:14:32 +0200

    fix panic on number to nil comparison (#34)
    * fix empty rules map that should return an empty json object instead of false
    
    * fix invalid comparisons to nil value #32
    
    * fix panic on number to nil comparison
    
    * fix panic on number to nil comparison
    
    Co-authored-by: Ken Polizzi <ken.polizzi@jaguar-network.com>

--- comp_test.go
+++ comp_test.go
@@ -10,6 +10,14 @@ func TestLessCompareStrings(t *testing.T) {
 	assert.True(t, less("a", "b"))
 }
 
+func TestLessCompareStringAndNil(t *testing.T) {
+	assert.False(t, less("a", nil))
+}
+
+func TestLessCompareNumberAndNil(t *testing.T) {
+	assert.False(t, less(12, nil))
+}
+
 func TestEqualsWithBooleans(t *testing.T) {
 	assert.True(t, equals(true, true))
 }
